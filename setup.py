cd1bf6a11152f23229539a90823bb874b09bf89d
Mayank <mynkpl1998@gmail.com>
2019-11-18 22:04:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='switch',
+    version='0.0.1',
+    description='Switch - A Gym compatible multi-agent environement',
+    py_modules=['switch'],
+    package_dir={'':'src'})
+
---
+++ src/switch.py
@@ -0,0 +1,9 @@
+import gym
+import pygame
+import numpy as np
+
+def helloWord(name=None):
+    if name is None:
+        print("hello mayank")
+    else:
+        print("hello ",name)
