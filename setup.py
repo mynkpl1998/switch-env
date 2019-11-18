cfd25238d195a154fc7d7985455cd53abf5f2064
Mayank <mynkpl1998@gmail.com>
2019-11-19 01:57:08 +0530

    minor edits in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup
 
 setup(name='switch',
     version='0.0.1',
-    description='Switch - A Gym compatible multi-agent environement',
-    py_modules=['switch'],
-    package_dir={'':'src'})
+    description='Switch - A multi-agent environement',
+    py_modules=['switch', 'common', 'constants', 'parser'],
+    package_dir={'':'src'},
+    author='Mayank Kumar Pal',
+    author_email='mayank15147@iiitd.ac.in',
+    python_requires='>=3.6.*',
+    project_urls={
+        'Source Code': 'https://github.com/mynkpl1998/switch-env'})
 
