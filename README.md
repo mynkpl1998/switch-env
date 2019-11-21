8b48407de55375b636ff45e9301dd58d177ea729
Mayank <mynkpl1998@gmail.com>
2019-11-21 19:57:19 +0530

    fixed build process

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Switch Environment
+
+## Installation Instructions
+
+Install dependencies via following command 
+
+`pip install -r requirements.txt`
+
+### Easy install
+### Build from source
+2. Install the dependensices
--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
+from src.common import readFile
+
+fileHandle = readFile("requirements.txt")
+description = readFile("README.md").read()
+install_requires = fileHandle.read().splitlines()
 
 setup(name='switch',
     version='0.0.1',
     description='Switch - A multi-agent environement',
-    py_modules=['switch', 'common', 'constants', 'parser'],
+    long_description=description,
+    long_description_content_type="text/markdown",
+    py_modules=['switch', 'common', 'constants', 'mapParser', 'ui'],
     package_dir={'':'src'},
     author='Mayank Kumar Pal',
+    install_requires=install_requires,
     author_email='mayank15147@iiitd.ac.in',
     python_requires='>=3.6.*',
     project_urls={
